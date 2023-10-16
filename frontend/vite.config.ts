3cebc2821c1358742f9ad2e6e920f8f574eee03a
Nghi Quyen <quyenkhanhnghi317@gmail.com>
2023-10-16 18:19:46 +0300

    fix: resolve vite emotion

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@emotion/react'],
+    include: ['@emotion/react', '@mui/styled-engine', '@emotion/cache'],
+  },
+  resolve: {
+    alias: {
+      '@emotion/react': require.resolve('@emotion/react'),
+      '@mui/styled-engine': require.resolve('@mui/styled-engine'),
+    },
   },
   build: {
     rollupOptions: {
