f979f1abb91df83c914b8c98b190fb77a6f20df0
Nghi Quyen <quyenkhanhnghi317@gmail.com>
2023-10-16 18:04:08 +0300

    feat: add Webpack 5 Mode for vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment',
+    jsxInject: `import React from 'react';`,
+  },
   build: {
     rollupOptions: {
       external: ['mapbox-gl/dist/mapbox-gl.css'],
