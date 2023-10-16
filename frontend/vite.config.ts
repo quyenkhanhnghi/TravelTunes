59b9b71c60ff6c8bb39a3a9b3d2abe3dba22ac60
Nghi Quyen <quyenkhanhnghi317@gmail.com>
2023-10-16 18:16:29 +0300

    fix: fix vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment',
+  optimizeDeps: {
+    include: ['@emotion/react'],
   },
   build: {
     rollupOptions: {
